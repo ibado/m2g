0f3e975b4a4863d8e4c61d89290a5686cbdc7c57
ibado <ignaciobado.unc@gmail.com>
2022-11-10 12:28:09 -0300

    translate single dependency happy case

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use roxmltree::Document;
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+
+    let out = maven_to_gradle(args[1].clone());
+
+    println!("\nfor gradle: \n\t{out}");
+}
+
+fn maven_to_gradle(input: String) -> String {
+    let curated = input.replace("\n", "").replace(" ", "");
+    let xml = match Document::parse(&curated) {
+        Ok(d) => d,
+        Err(e) => {
+            println!("error: {}", e);
+            std::process::exit(1)
+        }
+    };
+
+    let children_attrs: Vec<_> = xml
+        .root_element()
+        .children()
+        .filter_map(|child| child.text())
+        .collect();
+
+    let uri = children_attrs.join(":");
+    format!("implementation '{uri}'")
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::maven_to_gradle;
+
+    #[test]
+    fn translate_single_dependency() {
+        let input = String::from(
+            "
+<dependency>
+  <groupId>com.squareup.retrofit2</groupId>
+  <artifactId>retrofit</artifactId>
+  <version>34</version>
+</dependency>",
+        );
+
+        assert_eq!(
+            maven_to_gradle(input),
+            "implementation 'com.squareup.retrofit2:retrofit:34'"
+        )
+    }
+}
