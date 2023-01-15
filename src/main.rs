5f1a40d2329275458dff4c135e4c47e87c7164c1
ibado <ignaciobado.unc@gmail.com>
2023-01-15 22:34:48 +0100

    Support multiple dependencies

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,36 @@ fn main() {
     let args: Vec<_> = std::env::args().collect();
 
     match maven_to_gradle(args[1].clone()) {
-        Ok(g) => println!("\nfor gradle: \n\t{g}"),
+        Ok(g) => {
+            let r: Vec<_> = g.split("\n").into_iter().collect();
+            println!("\nfor gradle: \n\t{}", r.join("\n\t"));
+        }
         Err(e) => eprintln!("\nError: \n\t{e}"),
     };
 }
 
 fn maven_to_gradle(input: String) -> Result<String, String> {
-    let curated = input.replace("\n", "").replace(" ", "");
+    let curated = soround_str(input, "<p>", "</p>")
+        .replace("\n", "")
+        .replace(" ", "");
     let xml = Document::parse(&curated).map_err(|_| "Error parsing XML")?;
 
-    let root = xml.root_element();
-    if root.tag_name().name() == "dependency" {
-        let group = get_tag(&root, &"groupId")?;
-        let artifact = get_tag(&root, &"artifactId")?;
-        match get_tag(&root, &"version") {
+    let mut deps: Vec<String> = vec![];
+    for node in xml.root_element().children() {
+        let d = get_dep(&node)?;
+        deps.push(d);
+    }
+
+    Ok(deps.join("\n"))
+}
+
+fn get_dep<'a>(node: &Node<'a, 'a>) -> Result<String, String> {
+    if node.tag_name().name() == "dependency" {
+        let group = get_tag(node, "groupId")?;
+        let artifact = get_tag(node, "artifactId")?;
+        match get_tag(node, "version") {
             Ok(version) => Ok(format!("implementation \"{group}:{artifact}:{version}\"")),
-            Err(_) => Ok(format!("implementation \"{group}:{artifact}\""))
+            Err(_) => Ok(format!("implementation \"{group}:{artifact}\"")),
         }
     } else {
         Err("The provided string is not a correct maven dependency!".to_string())
@@ -29,9 +43,17 @@ fn maven_to_gradle(input: String) -> Result<String, String> {
 fn get_tag<'a>(root: &Node<'a, 'a>, name: &'static str) -> Result<&'a str, String> {
     root.children()
         .find(|child| child.tag_name().name() == name)
-        .ok_or(format!("Missing {name}"))?
+        .ok_or(format!("Missing {name}!"))?
         .text()
-        .ok_or(format!("Invalid content for {name}"))
+        .ok_or(format!("Invalid content for {name}!"))
+}
+
+fn soround_str(mut input: String, prefix: &str, suffix: &str) -> String {
+    let mut out = prefix.to_string();
+    input.push_str(suffix);
+    out.push_str(&input[..]);
+
+    out
 }
 
 #[cfg(test)]
@@ -88,6 +110,34 @@ mod tests {
         assert_eq!(
             maven_to_gradle(input).unwrap(),
             "implementation \"com.squareup.retrofit2:retrofit:${version}\""
+        )
+    }
+
+    #[test]
+    fn translate_multiple_dependencies() {
+        let input = String::from(
+            r#"
+            <dependency>
+                <groupId>com.dependencygroup</groupId>
+                <artifactId>dependency1</artifactId>
+                <version>1.0.0</version>
+            </dependency>
+            <dependency>
+                <groupId>com.dependencygroup</groupId>
+                <artifactId>dependency2</artifactId>
+                <version>0.1.2</version>
+            </dependency>
+            <dependency>
+                <groupId>com.dependencygroup</groupId>
+                <artifactId>dependency3</artifactId>
+                <version>2.1.0</version>
+            </dependency>
+            "#,
+        );
+
+        assert_eq!(
+            maven_to_gradle(input).unwrap(),
+            "implementation \"com.dependencygroup:dependency1:1.0.0\"\nimplementation \"com.dependencygroup:dependency2:0.1.2\"\nimplementation \"com.dependencygroup:dependency3:2.1.0\""
         )
     }
 }
